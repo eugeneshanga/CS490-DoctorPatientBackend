4290cb0188e29a8c08657f996cecf42a3720d9a7
Xin15943 <74322729+Xin15943@users.noreply.github.com>
2025-03-23 11:55:40 -0400

    Update auth.py

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -7,6 +7,7 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/api/auth")
 @auth_bp.route('/login', methods=['POST'])
 
 
+
 def login():
     data = request.get_json()
     email = data.get('email')
