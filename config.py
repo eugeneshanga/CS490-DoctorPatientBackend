8760c92a22f2162675ebc91fca79a8021697cc54
nickstern2002 <nicksternheim@gmail.com>
2025-05-07 23:09:55 -0400

    feat: setup charts and config for prod

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Root123!',
-    'database': 'weight_loss_clinic'
+    'host':     os.getenv('DB_HOST', 'localhost'),
+    'user':     os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASS', 'Root123!'),
+    'database': os.getenv('DB_NAME', 'weight_loss_clinic'),
 }
